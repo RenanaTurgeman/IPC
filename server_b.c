bdac49088869be07a1ca31efaf74ee55ab16aba4
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-03 11:16:55 +0300

    finish tcp and udp

--- server_b.c
+++ server_b.c
@@ -217,12 +217,75 @@ int ipv6_tcp() {
     return 0;
 }
 
+int ipv6_udp() {
+    int sockfd, connfd, filefd, nbytes;
+    struct sockaddr_in6 serv_addr, cli_addr;
+    socklen_t cli_len;
+    char buffer[BUFFER_SIZE];
+
+    // Create a socket for the server
+    sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    if (sockfd < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up the server address
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin6_family = AF_INET6;
+    serv_addr.sin6_addr = in6addr_any;
+    serv_addr.sin6_port = htons(SERVER_PORT);
+
+    // Bind the socket to the server address
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+   // Receive data from a client and write it to a file
+    cli_len = sizeof(cli_addr);
+    if ((nbytes = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &cli_addr, &cli_len)) < 0) {
+        perror("recvfrom");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open a file for writing
+    filefd = open("received_file.txt", O_CREAT | O_WRONLY, 0644);
+    if (filefd < 0) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Write data to the file
+    if (write(filefd, buffer, nbytes) != nbytes) {
+        perror("write");
+        exit(EXIT_FAILURE);
+    }
+
+    // Close the file and socket
+    close(filefd);
+    close(connfd);
+    close(sockfd);
+
+    // Print the contents of the file to the screen
+    FILE *fp = fopen("received_file.txt", "r");
+    if (fp == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+    }
+    fclose(fp);
+    return 0;
+}
 
 
 int main(int argc, char *argv[]) {
     // ipv4_tcp();
     // ipv4_udp();
-    ipv6_tcp();
-    
+    // ipv6_tcp();
+    ipv6_udp();
+
     return 0;
 }
--- tenp.c
+++ tenp.c
@@ -1,17 +1,11 @@
-int ipv6_tcp(){
+int ipv6_udp() {
     int sockfd, filefd, nbytes;
-    struct sockaddr_in6 serv_addr;
+    struct sockaddr_in6 serv_addr, cli_addr;
+    socklen_t cli_len;
     char buffer[BUFFER_SIZE];
 
-    // Open the file for reading
-    filefd = open("file.txt", O_RDONLY);
-    if (filefd < 0) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-
-    // Create a socket for the client
-    sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+    // Create a socket for the server
+    sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
     if (sockfd < 0) {
         perror("socket");
         exit(EXIT_FAILURE);
@@ -20,28 +14,48 @@ int ipv6_tcp(){
     // Set up the server address
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin6_family = AF_INET6;
+    serv_addr.sin6_addr = in6addr_any;
     serv_addr.sin6_port = htons(SERVER_PORT);
-    if (inet_pton(AF_INET6, SERVER_IP, &serv_addr.sin6_addr) <= 0) {
-        perror("inet_pton");
+
+    // Bind the socket to the server address
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    // Connect to the server
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        perror("connect");
-        exit(EXIT_FAILURE);
-    }
+    while (1) {
+        // Receive data from a client and write it to a file
+        cli_len = sizeof(cli_addr);
+        if ((nbytes = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &cli_addr, &cli_len)) < 0) {
+            perror("recvfrom");
+            exit(EXIT_FAILURE);
+        }
 
-    // Read from the file and send to the server
-    while ((nbytes = read(filefd, buffer, sizeof(buffer))) > 0) {
-        if (write(sockfd, buffer, nbytes) != nbytes) {
+        // Open a file for writing
+        filefd = open("received_file.txt", O_CREAT | O_WRONLY | O_APPEND, 0644);
+        if (filefd < 0) {
+            perror("open");
+            exit(EXIT_FAILURE);
+        }
+
+        // Write data to the file
+        if (write(filefd, buffer, nbytes) != nbytes) {
             perror("write");
+            exit(EXIT_FAILURE);
+        }
+
+        // Close the file
+        close(filefd);
+
+        // Send a response back to the client
+        if (sendto(sockfd, "OK", 2, 0, (struct sockaddr *) &cli_addr, sizeof(cli_addr)) < 0) {
+            perror("sendto");
             exit(EXIT_FAILURE);
         }
     }
 
-    // Close the file and socket
-    close(filefd);
+    // Close the socket (this line will never be executed)
     close(sockfd);
+
     return 0;
 }
