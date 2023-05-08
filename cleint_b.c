d9853952c292a5e9468630c64406f1d888c0ca42
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-08 09:59:19 +0300

    clean

--- cleint_b.c
+++ cleint_b.c
@@ -211,43 +211,6 @@ int ipv6_udp(){
 }
 
 int uds_dgram() {
-    // int sockfd, filefd, nbytes, n_sent;
-    // struct sockaddr_un serv_addr;
-    // char buffer[BUFSIZ];
-
-    // // Open the file for reading
-    // filefd = open("file.txt", O_RDONLY);
-    // if (filefd < 0) {
-    //     perror("open");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // // Create a socket for the client
-    // sockfd = socket(AF_UNIX, SOCK_DGRAM, 0);
-    // if (sockfd < 0) {
-    //     perror("socket");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // // Set up the server address
-    // memset(&serv_addr, 0, sizeof(serv_addr));
-    // serv_addr.sun_family = AF_UNIX;
-    // strncpy(serv_addr.sun_path, SERVER_SOCKET_PATH, sizeof(serv_addr.sun_path) - 1);
-
-    // // Read from the file and send to the server
-    // while ((nbytes = read(filefd, buffer, sizeof(buffer))) > 0) {
-    //     n_sent = sendto(sockfd, buffer, nbytes, 0, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-    //     if (n_sent < 0) {
-    //         perror("sendto");
-    //         exit(EXIT_FAILURE);
-    //     } else if (n_sent != nbytes) {
-    //         fprintf(stderr, "sendto: Sent %d bytes instead of %d bytes\n", n_sent, nbytes);
-    //     }
-    // }
-
-    // // Close the file and socket
-    // close(filefd);
-    // close(sockfd);
     int s, len;
     struct sockaddr_un remote = {
             .sun_family = AF_UNIX,
