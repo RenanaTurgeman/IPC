b78d7b5d9d82d69bc55d4ea54ce5cfe83fbecadf
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-09 23:02:32 +0300

    some fix

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     if(strcmp(argv[1], "-c") == 0)
     {
-        if(argc >= 5 && strcmp(argv[4], "-p"))
+        if(argc >= 5 && strcmp(argv[4], "-p")==0)
         {
             client_main_test(argc, argv);
         }
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     }
     else if(strcmp(argv[1], "-s") == 0)
     {
-        if(argc >= 5 && strcmp(argv[3], "-p"))
+        if(argc >= 5 && strcmp(argv[3], "-p")==0)
         {
             server_main_test(argc, argv);
         }
